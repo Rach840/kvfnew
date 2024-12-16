37cbefe07f80a4b9066cd32c120a8618c6fa78e9
Rach840 <rach.balayan1@gmail.com>
2024-12-17 01:34:40 +0300

    new

--- next.config.ts
+++ next.config.ts
@@ -6,5 +6,20 @@ const nextConfig: NextConfig = {
   }
   /* config options here */
 };
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: '@mdx-js/loader',
+          options: pluginOptions.options,
+        },
+      ],
+    })
 
+    return config
+  },
+}
 export default nextConfig;
