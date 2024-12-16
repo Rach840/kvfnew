d4074ac24210fa3105508e4aa565cf491c0d25c5
Rach840 <rach.balayan1@gmail.com>
2024-12-17 01:48:47 +0300

    n

--- next.config.ts
+++ next.config.ts
@@ -6,23 +6,5 @@ const nextConfig: NextConfig = {
   }
   /* config options here */
 };
-module.exports = {
-  reactStrictMode: true,
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: { and: [/\.(ts)x?$/] },
-      use: [
-        {
-          loader: "@svgr/webpack",
-          options: {
-            svgoConfig: { plugins: [{ removeViewBox: false }] },
-          },
-        },
-      ],
-    });
 
-    return config;
-  }
-}
 export default nextConfig;
