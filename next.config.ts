719d8a1e2bcf893063f937e98f47fa7157d5fe67
Rach840 <75145936+Rach840@users.noreply.github.com>
2024-12-18 08:19:51 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+output: 'export', images: { unoptimized: true, },
   /* config options here */
 };
-
+module.exports = nextConfig
 export default nextConfig;
